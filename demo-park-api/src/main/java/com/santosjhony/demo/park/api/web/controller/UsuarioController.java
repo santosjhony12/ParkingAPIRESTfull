e1efe0b0632de1cfabc8f2fb38853f0fe7b70ef5
santosjhony12 <santosdesouzajhony@gmail.com>
2023-12-27 17:59:41 -0300

    aula 27

--- demo-park-api/src/main/java/com/santosjhony/demo/park/api/web/controller/UsuarioController.java
+++ demo-park-api/src/main/java/com/santosjhony/demo/park/api/web/controller/UsuarioController.java
@@ -30,7 +30,7 @@ public class UsuarioController {
         return ResponseEntity.ok(UsuarioMapper.toDto(user));
     }
     @PatchMapping("/{id}")
-    public ResponseEntity<Void> updatePassword(@PathVariable Long id, @RequestBody UsuarioSenhaDto dto){
+    public ResponseEntity<Void> updatePassword(@PathVariable Long id, @Valid @RequestBody UsuarioSenhaDto dto){
         Usuario user = usuarioService.editarSenha(id, dto.getSenhaAtual(), dto.getNovaSenha(), dto.getConfirmaSenha());
         return ResponseEntity.noContent().build();
     }
