da3a10aee2dbd651a6909c2eae957e642198b404
santosjhony12 <santosdesouzajhony@gmail.com>
2024-02-02 19:54:00 -0300

    aula 59

--- demo-park-api/src/main/java/com/santosjhony/demo/park/api/jwt/JwtUserDetailsService.java
+++ demo-park-api/src/main/java/com/santosjhony/demo/park/api/jwt/JwtUserDetailsService.java
@@ -8,16 +8,18 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-@Service
 @RequiredArgsConstructor
+@Service
 public class JwtUserDetailsService implements UserDetailsService {
-    private UsuarioService usuarioService;
+
+    private final UsuarioService usuarioService;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Usuario usuario = usuarioService.buscarPorUsername(username);
         return new JwtUserDetails(usuario);
     }
-    public JwtToken getTokenAuthenticated(String username){
+
+    public JwtToken getTokenAuthenticated(String username) {
         Usuario.Role role = usuarioService.buscarRolePorUsername(username);
         return JwtUtils.createToken(username, role.name().substring("ROLE_".length()));
     }
