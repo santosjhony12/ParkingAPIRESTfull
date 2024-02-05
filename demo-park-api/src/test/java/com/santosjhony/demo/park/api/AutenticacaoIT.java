1325306f24274c1134412d8065d9f4b260787ad8
santosjhony12 <santosdesouzajhony@gmail.com>
2024-02-05 20:47:44 -0300

    aula 74

---
+++ demo-park-api/src/test/java/com/santosjhony/demo/park/api/AutenticacaoIT.java
@@ -0,0 +1,36 @@
+package com.santosjhony.demo.park.api;
+
+import com.santosjhony.demo.park.api.jwt.JwtToken;
+import com.santosjhony.demo.park.api.web.dto.UsuarioLoginDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = "/sql/usuarios/usuarios-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "/sql/usuarios/usuarios-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+public class AutenticacaoIT {
+
+    @Autowired
+    WebTestClient testClient;
+
+    @Test
+    public void autenticar_ComCredenciaisValidas_RetornarTokenComStatus200(){
+        JwtToken responseBody = testClient
+                .post()
+                .uri("/api/v1/auth")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UsuarioLoginDto("ana@gmail.com", "123456"))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(JwtToken.class)
+                .returnResult()
+                .getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+    }
+
+}
